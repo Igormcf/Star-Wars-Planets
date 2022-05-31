d5a5284ee36a618d99ba407b62f99a1c3b0f38e4
Igormcf <igormaciel82@gmail.com>
2022-05-31 11:53:55 -0300

    Updates

---
+++ precache-manifest.9d0326508564178652242183e79c33f9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691a0e2fa8a829db44324ace0f41d5c4",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "74443f2e57c9b0718b2e",
+    "url": "/star-wars-planets/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "64ef2e13706399860517",
+    "url": "/star-wars-planets/static/css/main.749a8207.chunk.css"
+  },
+  {
+    "revision": "74443f2e57c9b0718b2e",
+    "url": "/star-wars-planets/static/js/2.dee40ec9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planets/static/js/2.dee40ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ef2e13706399860517",
+    "url": "/star-wars-planets/static/js/main.333e27f4.chunk.js"
+  },
+  {
+    "revision": "089944949f54aeb36a92",
+    "url": "/star-wars-planets/static/js/runtime-main.d3fb0412.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/star-wars-planets/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "298f072e8de030aca69b6537f01e0125",
+    "url": "/star-wars-planets/static/media/cursor01.298f072e.png"
+  },
+  {
+    "revision": "6305c8dbeb55c7fd2f6c843a760fa552",
+    "url": "/star-wars-planets/static/media/cursor02.6305c8db.png"
+  },
+  {
+    "revision": "cb9eb44ef9a6e7c8f95996daabaeeb07",
+    "url": "/star-wars-planets/static/media/darth.cb9eb44e.mp3"
+  },
+  {
+    "revision": "9652b3f052466a96ebad3c42f6812ce0",
+    "url": "/star-wars-planets/static/media/distantgalaxy.9652b3f0.ttf"
+  },
+  {
+    "revision": "384340acedd821e899bc710eef65d041",
+    "url": "/star-wars-planets/static/media/logo.384340ac.png"
+  },
+  {
+    "revision": "51b7a05efa14a1fe463a7f7070aa95ab",
+    "url": "/star-wars-planets/static/media/sabre.51b7a05e.mp3"
+  },
+  {
+    "revision": "e856b266532e96bfb64ec8d8091b8e88",
+    "url": "/star-wars-planets/static/media/sabre2.e856b266.mp3"
+  },
+  {
+    "revision": "ee9364d55575aa2d6b8da450514ecdbf",
+    "url": "/star-wars-planets/static/media/starWars.ee9364d5.gif"
+  },
+  {
+    "revision": "2654b798209b5e02264fdf4b0cf7d3f6",
+    "url": "/star-wars-planets/static/media/theme.2654b798.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.bf7bc07069823a635027a2d5dad348b1.js"
+  "/star-wars-planets/precache-manifest.9d0326508564178652242183e79c33f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars-planets/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
