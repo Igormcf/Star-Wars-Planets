d85119a837196c0586cfc832d9b44320ae83928e
Igormcf <igormaciel82@gmail.com>
2022-05-31 11:42:37 -0300

    Updates

---
+++ precache-manifest.bf7bc07069823a635027a2d5dad348b1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8f6ca02e123e449b7ff91121199bc2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "74443f2e57c9b0718b2e",
+    "url": "/react-gh-pages/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "56b216620c0d0d7e6912",
+    "url": "/react-gh-pages/static/css/main.46650324.chunk.css"
+  },
+  {
+    "revision": "74443f2e57c9b0718b2e",
+    "url": "/react-gh-pages/static/js/2.dee40ec9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-gh-pages/static/js/2.dee40ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b216620c0d0d7e6912",
+    "url": "/react-gh-pages/static/js/main.e1a72822.chunk.js"
+  },
+  {
+    "revision": "75112a221d71c7565592",
+    "url": "/react-gh-pages/static/js/runtime-main.5f3a1a51.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/react-gh-pages/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "298f072e8de030aca69b6537f01e0125",
+    "url": "/react-gh-pages/static/media/cursor01.298f072e.png"
+  },
+  {
+    "revision": "6305c8dbeb55c7fd2f6c843a760fa552",
+    "url": "/react-gh-pages/static/media/cursor02.6305c8db.png"
+  },
+  {
+    "revision": "cb9eb44ef9a6e7c8f95996daabaeeb07",
+    "url": "/react-gh-pages/static/media/darth.cb9eb44e.mp3"
+  },
+  {
+    "revision": "9652b3f052466a96ebad3c42f6812ce0",
+    "url": "/react-gh-pages/static/media/distantgalaxy.9652b3f0.ttf"
+  },
+  {
+    "revision": "384340acedd821e899bc710eef65d041",
+    "url": "/react-gh-pages/static/media/logo.384340ac.png"
+  },
+  {
+    "revision": "51b7a05efa14a1fe463a7f7070aa95ab",
+    "url": "/react-gh-pages/static/media/sabre.51b7a05e.mp3"
+  },
+  {
+    "revision": "e856b266532e96bfb64ec8d8091b8e88",
+    "url": "/react-gh-pages/static/media/sabre2.e856b266.mp3"
+  },
+  {
+    "revision": "ee9364d55575aa2d6b8da450514ecdbf",
+    "url": "/react-gh-pages/static/media/starWars.ee9364d5.gif"
+  },
+  {
+    "revision": "2654b798209b5e02264fdf4b0cf7d3f6",
+    "url": "/react-gh-pages/static/media/theme.2654b798.mp3"
+  }
+]);
